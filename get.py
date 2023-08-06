51acf3c68f77e38ff1bb50456236bef868643e5f
KANOKIw <KUMANKENw@gmail.com>
2023-08-06 17:14:10 +0900

    hello

---
+++ get.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+url = "https://e-akane.com/blog/post-18198/"
+list = []
+
+
+def get(url):
+    d = 1
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, "html.parser")
+        td_texts = []
+        for td in soup.find_all("td"):
+            d += 1
+            if d%2 == 0:
+                td_texts.append(td.get_text())
+        return td_texts
+    else:
+        print(f"Load failed!: {response.status_code}")
+        return []
+
+if __name__ == "__main__":
+    sentences = get(url)
+
+    with open(r"src\main\java\si\f5\mitminecraft\sushida\sushis.json", "w", encoding="utf-8") as f:
+        json.dump(sentences, f, indent=4, ensure_ascii=False)
---
+++ src/main/java/si/f5/mitminecraft/sushida/Main.java
@@ -0,0 +1,28 @@
+package si.f5.mitminecraft.sushida;
+
+import java.io.IOException;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class Main extends JavaPlugin {
+    public static Main plugin;
+
+    public Main() throws IOException {
+        plugin = this;
+    }
+
+    @Override
+    public void onEnable(){
+        getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable(){
+            @Override
+            public void run() {
+                Sushida.removeCd();
+            }
+        }, 0L, 20L);
+		getCommand("typing").setExecutor(new Sushida());
+        getCommand("entyping").setExecutor(new Engda());
+        getCommand("sushida").setExecutor(new Start());
+        getLogger().info("< Sushida loaded >");
+    }
+}
