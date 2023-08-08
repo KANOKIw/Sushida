42566944510dc79a3816503b83456a8e87c61957
KANOKIw <123856636+KANOKIw@users.noreply.github.com>
2023-08-08 19:15:42 +0900

    Add files via upload

--- get.py
+++ get.py
@@ -3,24 +3,27 @@ from bs4 import BeautifulSoup
 import json
 
 url = "https://e-akane.com/blog/post-18198/"
+list = []
+
 
 def get(url):
-    d = 1
     response = requests.get(url)
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, "html.parser")
-        td_texts = []
+        l = []
         for td in soup.find_all("td"):
-            d += 1
-            if d%2 == 0:
-                td_texts.append(td.get_text())
-        return td_texts
+            td = str(td)
+            if not td.startswith('<td height="18" style="height: 13.5pt;">'):continue
+            sushi = td.replace('<td height="18" style="height: 13.5pt;">', "").replace("</td>", "")
+            if (len(p) > 8):continue
+            l.append(sushi)
+        return l
     else:
         print(f"Load failed!: {response.status_code}")
         return []
 
 if __name__ == "__main__":
-    list = get(url)
+    sentences = get(url)
 
-    with open(r"src\main\java\si\f5\mitminecraft\resources\sushis.json", "w", encoding="utf-8") as f:
-        json.dump(list, f, indent=4, ensure_ascii=False)
+    with open(r"src\main\java\si\f5\mitminecraft\sushida\sushis.json", "w", encoding="utf-8") as f:
+        json.dump(sentences, f, indent=4, ensure_ascii=False)
